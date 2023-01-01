246b6c407b8b5cd1521cf6af0122d891727859d7
DenysZyrianov <78175281+Denys1802@users.noreply.github.com>
2023-01-01 20:36:29 +0200

    add gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector('.gallery');
+const createGallery = createGalleryItemsMurkup(galleryItems);
+
+galleryContainerEl.innerHTML = createGallery;
+
+function createGalleryItemsMurkup(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
