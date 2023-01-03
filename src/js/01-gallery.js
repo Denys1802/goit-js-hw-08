a3d35ab0c77bea6ecc0e516f86f87a2e57318abe
DenysZyrianov <78175281+Denys1802@users.noreply.github.com>
2023-01-04 01:38:26 +0200

    added gallery-02 and 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,11 +2,7 @@ import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
-console.log(galleryItems);
 
 const galleryContainerEl = document.querySelector('.gallery');
 const createGallery = createGalleryItemsMurkup(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+//window.addEventListener('touchstart', { passive: true });
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+//console.log(player);
+const onPlay = function (data) {
+  console.log(data);
+  try {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
 
+const throtlePlay = throttle(onPlay, 1000);
+player.on('timeupdate', throtlePlay);
+
+const getCurrentPlayTime = function () {
+  try {
+    const getStorage = localStorage.getItem('videoplayer-current-time');
+    const parseStorage = JSON.parse(getStorage);
+    return parseStorage.seconds;
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+player.setCurrentTime(getCurrentPlayTime());
