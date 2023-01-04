bf6f033b0afd9f9ab1057faaee9e8dba1b00dd85
DenysZyrianov <78175281+Denys1802@users.noreply.github.com>
2023-01-04 15:53:23 +0200

    added 02 and 03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,12 +3,10 @@ import throttle from 'lodash.throttle';
 //window.addEventListener('touchstart', { passive: true });
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-//console.log(player);
 
 const onPlay = function (data) {
-  console.log(data);
   try {
-    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+    localStorage.setItem('videoplayer-current-time', data.seconds);
   } catch (error) {
     console.error('Set state error: ', error.message);
   }
@@ -17,14 +15,13 @@ const onPlay = function (data) {
 const throtlePlay = throttle(onPlay, 1000);
 player.on('timeupdate', throtlePlay);
 
-const getCurrentPlayTime = function () {
-  try {
-    const getStorage = localStorage.getItem('videoplayer-current-time');
-    const parseStorage = JSON.parse(getStorage);
-    return parseStorage.seconds;
-  } catch (error) {
-    console.error('Get state error: ', error.message);
-  }
-};
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
 
-player.setCurrentTime(getCurrentPlayTime());
+// player.on(
+//   'timeupdate',
+//   throttle(function (time) {
+//     localStorage.setItem('videoplayer-current-time', time.seconds);
+//   }, 1000)
+// );
+
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', e => {
+  const input = e.currentTarget[0].value;
+  const textarea = e.currentTarget[1].value;
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({ email: input, message: textarea })
+  );
+});
+// form.addEventListener('submit', onFormsubmit);
+// function onFormsubmit(event) {
+//   event.preventDefault();
+//   const formElements = event.currentTarget.elements;
+//   const email = formElements.email.value;
+//   const password = formElements.password.value;
+//   if (email === '' || password === '') {
+//     alert('Всі поля повинні бути заповнені!!!');
+//   } else {
+//     const formData = {
+//       email,
+//       password,
+//     };
+//     console.log(formData);
+//     form.reset();
+//   }
+// }
 
+// form.addEventListener('submit', handleSubmit);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { login, password },
+//   } = event.currentTarget;
+
+//   if (login.value === '' || password.value === '') {
+//     return console.log('Please fill in all the fields!');
+//   }
+
+//   console.log(`Login: ${login.value}, Password: ${password.value}`);
+//   event.currentTarget.reset();
+// }
