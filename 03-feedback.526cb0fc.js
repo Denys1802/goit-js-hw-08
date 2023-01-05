2f7994625204103bd722b0366ee2c66169e535f2
Denys1802 <78175281+Denys1802@users.noreply.github.com>
2023-01-05 09:41:26 +0000

    Deploying to gh-pages from @ Denys1802/goit-js-hw-08@c6d706b2acd3aaf50acef5f3d4ca174fffc49214 🚀

--- 03-feedback.526cb0fc.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form").addEventListener("input",(function(e){var t=e.currentTarget[0].value,r=e.currentTarget[1].value;localStorage.setItem("feedback-form-state",JSON.stringify({email:t,message:r}))}));
-//# sourceMappingURL=03-feedback.526cb0fc.js.map
